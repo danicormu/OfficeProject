1c596703958e1f296330927dcecec9a0bec1eb20
danicormu <danicormu@gmail.com>
2017-11-17 22:36:03 -0600

    Changes, Entity Framework implementation

--- OfficeProj/OfficeProj/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OfficeProj.MvcApplication" Language="C#" %>
---
+++ OfficeProject/OfficeProject/Controllers/CursoController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OfficeProject.Models;
+
+namespace OfficeProject.Controllers
+{
+    public class CursoController : Controller
+    {
+        // GET: Curso
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult List()
+        {
+            try
+            {
+                using (pruebaDBEntities db = new pruebaDBEntities())
+                {
+                    List<Curso> list = db.Cursos.ToList();
+                    return View(list);
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public ActionResult Create(Curso curso)
+        {
+            try
+            {
+                using (pruebaDBEntities db = new pruebaDBEntities)
+                {
+                    db.Cursos.Add(curso);
+                    db.SaveChanges();
+                    return RedirectToAction("List");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("Error al agregar el Curso", ex);
+                return View();
+            }
+        }
+    }
+}
